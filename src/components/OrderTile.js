a7840543c7f5c65420ac681e692a3f2a0cd953ff
timolor <timolordave@gmail.com>
2020-10-14 02:28:01 +0100

    orders tiles screens

---
+++ src/components/OrderTile.js
@@ -0,0 +1,163 @@
+import React, { Component } from "react";
+import { SafeAreaView, StyleSheet, Text, Image, View, Dimensions, TouchableWithoutFeedback, TouchableOpacity } from 'react-native';
+import { Icon } from 'react-native-elements';
+
+
+class OrderTile extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isPressed: false,
+            tabType: props.tabType,
+            actionText: ""
+        }
+    }
+
+    componentDidMount() {
+        this.setState({ isPressed: false })
+        this._handleTabClicked();
+    }
+
+
+
+    _handleTabClicked() {
+        if (this.props.tabType === 'completed')
+            this.setState({ actionText: "" })
+        if (this.props.tabType === 'pending')
+            this.setState({ actionText: "Order Picked up" })
+        if (this.props.tabType === 'inProgress')
+            this.setState({ actionText: "Delivery Completed" })
+    }
+
+    render() {
+        let dropDown;
+        if (this.state.isPressed) {
+            //if (this.state.tabType === 'pending' || this.state.tabType === 'inProgress') {
+            dropDown =
+                <View >
+                    <View style={{ paddingHorizontal: 40, paddingTop: 20 }}>
+                        <Text style={styles.ddTitle}>Sender's name</Text>
+                        <Text style={styles.ddValue}>Ginika</Text>
+                        <Text style={styles.ddTitle}>Sender's phone number</Text>
+                        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                            <Text style={styles.ddValue}>+234 708 525 4825</Text>
+                            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                                <Icon
+                                    active
+                                    name='call' size={25} color={'#0ED91B'}
+                                    type='ionicons' iconStyle={{ paddingHorizontal: 10 }} />
+                                <Icon
+                                    active
+                                    name='message' size={25} color={'#0ED91B'}
+                                    type='material-icons' iconStyle={{ paddingHorizontal: 10 }} />
+                            </View>
+                        </View>
+                        <Text style={styles.ddTitle}>Pickup address</Text>
+                        <Text style={styles.ddValue}>18 Rumens road, Ikoyi, Lagos</Text>
+                        <Text style={styles.ddTitle}>Description</Text>
+                        <Text style={styles.ddValue}>Clothing</Text>
+                    </View>
+                    <View style={{ paddingHorizontal: 40, paddingTop: 10 }}>
+                        <Text style={styles.ddTitle}>Receiver's name</Text>
+                        <Text style={styles.ddValue}>Ginika</Text>
+                        <Text style={styles.ddTitle}>Receiver's phone number</Text>
+                        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                            <Text style={styles.ddValue}>+234 708 525 4825</Text>
+                            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                                <Icon
+                                    active
+                                    name='call' size={25} color={'#0ED91B'}
+                                    type='ionicons' iconStyle={{ paddingHorizontal: 10 }} />
+                                <Icon
+                                    active
+                                    name='message' size={25} color={'#0ED91B'}
+                                    type='material-icons' iconStyle={{ paddingHorizontal: 10 }} />
+                            </View>
+                        </View>
+                        <Text style={styles.ddTitle}>Delivery address</Text>
+                        <Text style={styles.ddValue}>3 Morinre Johnson, Lekki Phase 1, Lagos</Text>
+                    </View>
+                    <View style={{ flexDirection: 'row', paddingVertical: 30, paddingHorizontal: 40, backgroundColor: '#0ED91B', marginTop: 10, justifyContent: 'space-between', alignItems: 'center', marginBottom: 40 }}>
+                        <View style={{ flexDirection: 'row' }}>
+                            <Icon
+                                active
+                                name='double-arrow' size={25} color={'#FFF'}
+                                type='material-icons' />
+                        </View>
+                        <Text style={{ fontSize: 20, color: '#FFF' }}>{this.state.actionText}</Text>
+                        <View style={{ flexDirection: 'row' }}>
+                            <Icon
+                                active
+                                name='double-arrow' size={25} color={'#FFF'}
+                                type='material-icons' />
+                        </View>
+                    </View>
+                </View>
+            //}
+        }
+        return (
+            <View style={this.props.style}>
+                <View style={styles.cardContainer}>
+                    <TouchableOpacity style={{ paddingHorizontal: 10 }} onPress={() => { this.setState({ isPressed: !this.state.isPressed }); this._handleTabClicked() }}>
+                        <Text style={{ fontWeight: "bold", fontSize: 18, marginLeft: 25 }}>Order #</Text>
+                        <View style={styles.orderTab}>
+                            <View style={{ flex: 1, justifyContent: 'space-between' }}>
+                                <Icon
+                                    active
+                                    name='dot-single' size={25} color={'#0ED91B'}
+                                    type='entypo' />
+
+                                <Icon
+                                    active
+                                    name='dot-single' size={25} color={'#707070'}
+                                    type='entypo' />
+
+                            </View>
+                            <View style={{ flex: 3, }}>
+                                <Text style={{ color: '#F72514', }}>Festac Town, Lagos</Text>
+                                <Text>to</Text>
+                                <Text style={{ color: '#F72514' }}>Idumota, Lagos</Text>
+                            </View>
+                            <View style={{ flex: 4, justifyContent: 'space-between' }}>
+                                <Text>11 Mar 2020, 11:26:10</Text>
+                                <Text>11 Mar 2020, 12:01:16</Text>
+                            </View>
+                            <View style={{ flex: 3, alignItems: 'center', justifyContent: 'center' }}>
+                                <Text>₦2000</Text>
+                            </View>
+                        </View>
+                    </TouchableOpacity>
+                    {dropDown}
+                </View>
+            </View>
+        );
+    }
+
+}
+
+export default OrderTile;
+
+const styles = StyleSheet.create({
+    cardContainer: {
+        flex: 1,
+        flexDirection: 'column',
+        borderBottomColor: '#EAEAEA',
+        borderBottomWidth: 3,
+        paddingBottom: 10
+    },
+    orderTab: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'stretch'
+    },
+    ddTitle: {
+        color: '#707070',
+        fontSize: 12
+    },
+    ddValue: {
+        color: '#000',
+        fontSize: 16
+    }
+
+});
--- src/scenes/home/DeliveriesScreen.js
+++ src/scenes/home/DeliveriesScreen.js
@@ -1,22 +1,131 @@
 
 import React, { Component, useState, useEffect } from 'react';
-import { StyleSheet, View, Text, Image } from 'react-native';
+import { StyleSheet, View, Text, Image, TouchableOpacity, FlatList } from 'react-native';
+import OrderTile from '../../components/OrderTile';
 
-export default function DeliveriesScreen({ navigation }) {
+export default class DeliveriesScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tab: "completed",
+            completedOrders: [
+                "", "",
+            ],
+            ordersInPorgress: [
+                "", "", ""
+            ],
+            pendingOrders: [
+                "",
+            ],
+            orderList: [],
+            completed: { borderBottomColor: 'red' },
+            inProgress: { borderBottomColor: 'transparent' },
+            pending: { borderBottomColor: 'transparent' }
+        }
+    }
 
+    _handleTabClick(type) {
 
-    return (
-        <View style={styles.container}>
-             <Text>You have no pending orders now</Text>
-        </View>
-    );
+        this.setState({ tab: type })
+        if (type === 'completed') {
+            this.setState({
+                completed: { borderBottomColor: 'red' }, inProgress: { borderBottomColor: 'transparent' }, pending: { borderBottomColor: 'transparent' },
+                orderList: ["", ""]
+            })
+        }
+        if (type === 'inProgress') {
+            this.setState({
+                completed: { borderBottomColor: 'transparent' }, inProgress: { borderBottomColor: 'red' }, pending: { borderBottomColor: 'transparent' },
+                orderList: ["", "", ""]
+            })
+        }
+        if (type === 'pending') {
+            this.setState({
+                completed: { borderBottomColor: 'transparent' }, inProgress: { borderBottomColor: 'transparent' }, pending: { borderBottomColor: 'red' },
+                orderList: [""]
+            })
+        }
+    }
+
+    render() {
+        let body
+        if (this.state.tab === 'completed') {
+            body = <View>
+                <FlatList
+                    style={{ paddingBottom: 5 }}
+                    data={this.state.completedOrders}
+                    extraData={this.state}
+                    renderItem={({ item }) => (
+                        <View style={{ flex: 1, flexDirection: 'column', margin: 2, }}>
+                            <OrderTile tabType={this.state.tab} />
+                        </View>
+                    )}
+                    keyExtractor={(item, index) => index.toString()}
+                />
+            </View>
+
+        }
+        if (this.state.tab === 'inProgress') {
+            body = <View>
+                <FlatList
+                    style={{ paddingBottom: 5 }}
+                    data={this.state.ordersInPorgress}
+                    extraData={this.state}
+                    renderItem={({ item }) => (
+                        <View style={{ flex: 1, flexDirection: 'column', margin: 2, }}>
+                            <OrderTile tabType={this.state.tab} />
+                        </View>
+                    )}
+                    keyExtractor={(item, index) => index.toString()}
+                />
+            </View>
+
+        }
+        if (this.state.tab === 'pending') {
+            body = <View>
+                <FlatList
+                    style={{ paddingBottom: 5 }}
+                    data={this.state.pendingOrders}
+                    extraData={this.state}
+                    renderItem={({ item }) => (
+                        <View style={{ flex: 1, flexDirection: 'column', margin: 2, }}>
+                            <OrderTile tabType={this.state.tab} />
+                        </View>
+                    )}
+                    keyExtractor={(item, index) => index.toString()}
+                />
+            </View>
+
+        }
+        return (
+            <View style={styles.container}>
+                <View style={styles.header}>
+                    <TouchableOpacity style={[{ borderBottomWidth: 4, paddingVertical: 20, paddingHorizontal: 20 }, this.state.completed]} onPress={() => this._handleTabClick('completed')}><Text style={styles.headerText}>Completed</Text></TouchableOpacity>
+                    <TouchableOpacity style={[{ borderBottomWidth: 4, paddingVertical: 20, paddingHorizontal: 20 }, this.state.inProgress]} onPress={() => this._handleTabClick('inProgress')}><Text style={styles.headerText}>In Progress</Text></TouchableOpacity>
+                    <TouchableOpacity style={[{ borderBottomWidth: 4, paddingVertical: 20, paddingHorizontal: 20 }, this.state.pending]} onPress={() => this._handleTabClick('pending')}><Text style={styles.headerText}>Pending</Text></TouchableOpacity>
+                </View>
+                {body}
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#FFF'
+        flex: 1, backgroundColor: '#FFF'
     },
     title: {
         fontSize: 35, textTransform: "uppercase", marginTop: 10
+    },
+    header: {
+        backgroundColor: '#2AA1B5',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    headerText: {
+        color: '#FFF',
+        fontWeight: "bold",
+        fontSize: 18
     }
-});
+}); 
