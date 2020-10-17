5961c64f019d3df619939f226ae48f2365df4ad1
timolor <timolordave@gmail.com>
2020-10-17 22:53:06 +0100

    read and send device location

--- src/navigations/auth-navigator.js
+++ src/navigations/auth-navigator.js
@@ -4,11 +4,13 @@ import VerificationScreen from '../scenes/auth';
 import OtpScreen from '../scenes/auth/OtpScreen';
 import AppNavigator from './app-navigator';
 import IncomingOrderScreen from '../scenes/home/IncomingRequestScreen';
+import BgTracking from '../services/BgTracking';
 
 const AuthStack = createStackNavigator();
 function AuthNavigator() {
     return (
         <AuthStack.Navigator>
+          <AuthStack.Screen options={{ headerShown: false }} name="BgTracking" component={BgTracking} />
            <AuthStack.Screen options={{ headerShown: false }} name="IncomingOrder" component={IncomingOrderScreen} />
             <AuthStack.Screen options={{ headerShown: false }} name="Verification" component={VerificationScreen} />
             <AuthStack.Screen options={{ headerShown: false }} name="OTP" component={OtpScreen} />
--- src/services/BgTracking.js
+++ src/services/BgTracking.js
@@ -1,30 +1,8 @@
-
-import React, { Component, useState, useEffect } from 'react';
-import { StyleSheet, View, Text, Image, TouchableOpacity, FlatList } from 'react-native';
-import OrderTile from '../../components/OrderTile';
+import React, { Component } from 'react';
+import { Alert, Text, View } from 'react-native';
 import BackgroundGeolocation from '@mauron85/react-native-background-geolocation';
 
-export default class DeliveriesScreen extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            tab: "completed",
-            completedOrders: [
-                "", "",
-            ],
-            ordersInPorgress: [
-                "", "", ""
-            ],
-            pendingOrders: [
-                "",
-            ],
-            orderList: [],
-            completed: { borderBottomColor: 'red' },
-            inProgress: { borderBottomColor: 'transparent' },
-            pending: { borderBottomColor: 'transparent' }
-        }
-    }
-
+class BgTracking extends Component {
     componentDidMount() {
         BackgroundGeolocation.configure({
             desiredAccuracy: BackgroundGeolocation.HIGH_ACCURACY,
@@ -36,9 +14,9 @@ export default class DeliveriesScreen extends Component {
             startOnBoot: false,
             stopOnTerminate: true,
             locationProvider: BackgroundGeolocation.ACTIVITY_PROVIDER,
-            interval: 10000,
-            fastestInterval: 5000,
-            activitiesInterval: 10000,
+            interval: 600000,
+            fastestInterval: 300000,
+            activitiesInterval: 600000,
             stopOnStillActivity: false,
             url: 'https://airandapi.azurewebsites.net/api/location/driver/update',
             httpHeaders: {
@@ -129,6 +107,11 @@ export default class DeliveriesScreen extends Component {
 
         // you can also just start without checking for status
         // BackgroundGeolocation.start();
+
+
+        setTimeout(() => {
+            this.props.navigation.navigate('IncomingOrder');
+        }, 15000);
     }
 
     sendLocation(location) {
@@ -151,108 +134,19 @@ export default class DeliveriesScreen extends Component {
         BackgroundGeolocation.removeAllListeners();
     }
 
-    _handleTabClick(type) {
-
-        this.setState({ tab: type })
-        if (type === 'completed') {
-            this.setState({
-                completed: { borderBottomColor: 'red' }, inProgress: { borderBottomColor: 'transparent' }, pending: { borderBottomColor: 'transparent' },
-                orderList: ["", ""]
-            })
-        }
-        if (type === 'inProgress') {
-            this.setState({
-                completed: { borderBottomColor: 'transparent' }, inProgress: { borderBottomColor: 'red' }, pending: { borderBottomColor: 'transparent' },
-                orderList: ["", "", ""]
-            })
-        }
-        if (type === 'pending') {
-            this.setState({
-                completed: { borderBottomColor: 'transparent' }, inProgress: { borderBottomColor: 'transparent' }, pending: { borderBottomColor: 'red' },
-                orderList: [""]
-            })
-        }
-    }
-
     render() {
-        let body
-        if (this.state.tab === 'completed') {
-            body = <View>
-                <FlatList
-                    style={{ paddingBottom: 5 }}
-                    data={this.state.completedOrders}
-                    extraData={this.state}
-                    renderItem={({ item }) => (
-                        <View style={{ flex: 1, flexDirection: 'column', margin: 2, }}>
-                            <OrderTile tabType={this.state.tab} />
-                        </View>
-                    )}
-                    keyExtractor={(item, index) => index.toString()}
-                />
-            </View>
-
-        }
-        if (this.state.tab === 'inProgress') {
-            body = <View>
-                <FlatList
-                    style={{ paddingBottom: 5 }}
-                    data={this.state.ordersInPorgress}
-                    extraData={this.state}
-                    renderItem={({ item }) => (
-                        <View style={{ flex: 1, flexDirection: 'column', margin: 2, }}>
-                            <OrderTile tabType={this.state.tab} />
-                        </View>
-                    )}
-                    keyExtractor={(item, index) => index.toString()}
-                />
-            </View>
-
-        }
-        if (this.state.tab === 'pending') {
-            body = <View>
-                <FlatList
-                    style={{ paddingBottom: 5 }}
-                    data={this.state.pendingOrders}
-                    extraData={this.state}
-                    renderItem={({ item }) => (
-                        <View style={{ flex: 1, flexDirection: 'column', margin: 2, }}>
-                            <OrderTile tabType={this.state.tab} />
-                        </View>
-                    )}
-                    keyExtractor={(item, index) => index.toString()}
-                />
-            </View>
-
-        }
         return (
-            <View style={styles.container}>
-                <View style={styles.header}>
-                    <TouchableOpacity style={[{ borderBottomWidth: 4, paddingVertical: 20, paddingHorizontal: 20 }, this.state.completed]} onPress={() => this._handleTabClick('completed')}><Text style={styles.headerText}>Completed</Text></TouchableOpacity>
-                    <TouchableOpacity style={[{ borderBottomWidth: 4, paddingVertical: 20, paddingHorizontal: 20 }, this.state.inProgress]} onPress={() => this._handleTabClick('inProgress')}><Text style={styles.headerText}>In Progress</Text></TouchableOpacity>
-                    <TouchableOpacity style={[{ borderBottomWidth: 4, paddingVertical: 20, paddingHorizontal: 20 }, this.state.pending]} onPress={() => this._handleTabClick('pending')}><Text style={styles.headerText}>Pending</Text></TouchableOpacity>
-                </View>
-                {body}
+
+            <View>
+
+
+                <Text>Location</Text>
+
+
             </View>
-        );
+
+        )
     }
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1, backgroundColor: '#FFF'
-    },
-    title: {
-        fontSize: 35, textTransform: "uppercase", marginTop: 10
-    },
-    header: {
-        backgroundColor: '#2AA1B5',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-    },
-    headerText: {
-        color: '#FFF',
-        fontWeight: "bold",
-        fontSize: 18
-    }
-}); 
+export default BgTracking;
